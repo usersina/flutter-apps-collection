9a5f60b3f8693cdd0aedb22fc235f526737d4ead
usersina <benna.anis2@gmail.com>
2021-11-02 09:55:14 +0100

    Add state to fetch json file

--- json_handler/lib/screens/main_screen.dart
+++ json_handler/lib/screens/main_screen.dart
@@ -1,7 +1,23 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String? rawData;
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile();
+    // -- https://stackoverflow.com/questions/58400001/should-i-call-super-initstate-at-the-end-or-at-the-beginning
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +25,18 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("JSON Handler"),
       ),
-      body: const Text("Base project"),
+      body: Text(
+        rawData != null ? rawData! : "",
+      ),
     );
+  }
+
+  Future readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(context).loadString(
+      "assets/pizzalist.json",
+    );
+    setState(() {
+      rawData = myString;
+    });
   }
 }
