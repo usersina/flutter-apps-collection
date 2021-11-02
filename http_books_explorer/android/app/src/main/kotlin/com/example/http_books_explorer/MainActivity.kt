9c51eb94c267c5b9ad2502c2fd084174a44e02dd
usersina <benna.anis2@gmail.com>
2021-11-02 10:49:07 +0100

    Generate base books explorer

---
+++ http_books_explorer/android/app/src/main/kotlin/com/example/http_books_explorer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.http_books_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ http_books_explorer/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http_books_explorer/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Books Explorer",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
