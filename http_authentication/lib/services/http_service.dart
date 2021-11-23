ffb635684cbc9b5ae7c7c46531cd40a98f808962
usersina <benna.anis2@gmail.com>
2021-11-23 10:20:58 +0100

    Create untested http service

--- http_authentication/lib/services/http_service.dart
+++ http_authentication/lib/services/http_service.dart
@@ -1,1 +1,33 @@
-class HttpService {}
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class HttpService {
+  final String baseUrl = "https://spring-app-url:8080/";
+
+  // -- TESTME:
+  Future<dynamic> registerWithEmailAndPassword(
+      String email, String password) async {
+    dynamic res = await post(
+      Uri.parse(baseUrl + "register"),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(
+        {'email': email, 'password': password},
+      ),
+    );
+    return res.body;
+  }
+
+  // -- TESTME:
+  Future<dynamic> signInWithEmailAndPassword(
+      String email, String password) async {
+    dynamic res = await post(
+      Uri.parse(baseUrl + "login"),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(
+        {'email': email, 'password': password},
+      ),
+    );
+    return res.body;
+  }
+}
