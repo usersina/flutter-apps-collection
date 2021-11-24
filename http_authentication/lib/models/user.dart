f74da45e52195601c0a03b7d305c55c33c24773f
usersina <benna.anis2@gmail.com>
2021-11-24 22:59:37 +0100

    Add user & user provider classes

---
+++ http_authentication/lib/models/user.dart
@@ -0,0 +1,5 @@
+class User {
+  String email;
+
+  User(this.email);
+}
---
+++ http_authentication/lib/providers/user_provider.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http_authentication/models/user.dart';
+
+class UserProvider extends ChangeNotifier {
+  final _streamController = StreamController<User?>();
+
+  Stream<User?> get loggedUser$ {
+    return _streamController.stream;
+  }
+}
