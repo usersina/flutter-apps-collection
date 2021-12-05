d8d41bb3c5216931cefa0cf397a39c10b2279b7e
usersina <benna.anis2@gmail.com>
2021-12-05 09:26:47 +0100

    Initialize flutter app

---
+++ client_app/sol_project/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:sol_project/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Firestore Products Manager",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
---
+++ client_app/sol_project/lib/screens/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Spark of Light"),
+      ),
+      body: const Text("test"),
+    );
+  }
+}
